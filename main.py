b0d09cab6f8ed44c3f08936b684241fed6e6a8ba
Nathan Strong <87103168+NathanNull@users.noreply.github.com>
2022-04-24 20:35:52 -0700

    Added support for larger boards

--- main.py
+++ main.py
@@ -5,7 +5,11 @@
 # algorithm (look it up idk) to solve sudoku puzzles
 
 import copy
+import math
+from multiprocessing.sharedctypes import Value
 import random
+
+digits = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 class square(list[int]):
     def __init__(self, allowed_vals:list[int]) -> None:
@@ -25,44 +29,71 @@ class square(list[int]):
         return not (self == __o)
 
 class board(list[list[square]]):
-    def __init__(self, known:list[list[int]]=None) -> None:
-        """known: 9x9 grid with -1 for each unknown element"""
+    def __init__(self, known:list[list[int]]=None, size:int=9) -> None:
+        """known: grid with -1 for each unknown element\n
+        size: number of numbers tall the grid is, use if you don't know anything. Needs to be square."""
+        if known == None:
+            known = [[-1 for _ in range(size)] for _ in range(size)]
+        else:
+            size = len(known)
+        self.size = size
+        self.gridsize = int(math.sqrt(size))
+        if self.gridsize != math.sqrt(size):
+            raise ValueError(f"Size {size} needs to be a square number")
+
         self.clear()
-        for y in range(9):
-            self.append([square([i for i in range(1,10) if (known[y][x] in (i, -1))]) for x in range(9)])
+        for y in range(size):
+            self.append([square([i for i in range(1,size+1) if (known[y][x] in (i, -1))]) for x in range(size)])
         self.propagate()
     
     def __str__(self) -> str:
         """Print out the whole board in a legible format"""
         string_lines = ["-"*73]
         for line in self:
-            for i in range(3):
+            for i in range(self.gridsize):
                 string_line = "|"
                 for sqr in line:
                     string_line = string_line + " "
-                    for j in range(3):
-                        string_line = f"{string_line}{i*3+j+1 if i*3+j+1 in sqr else ('_' if sqr.solved else ' ')} "
+                    for j in range(self.gridsize):
+                        # Selects character based on whether solved, possible, or ruled out
+                        string_line = f"{string_line}\
+{digits[i*self.gridsize+j] if i*self.gridsize+j+1 in sqr else ('_' if sqr.solved else ' ')} "
                     string_line = string_line[:-1]+" |"
                 string_lines.append(string_line)
             string_lines.append("-"*73)
         return "\n".join(string_lines)
     
+    def __format__(self, __format_spec: str) -> str:
+        if __format_spec == "compact":
+            lines = []
+            for line in self:
+                l = ""
+                for sqr in line:
+                    l = l+(digits[sqr[0]-1] if sqr.solved else "?")
+                lines.append(l)
+            return "\n".join(lines)
+        else:
+            return str(self)
+    
     def isvalid(self, x:int, y:int, num:int) -> bool:
         """Tests if a certain number is valid in a certain position"""
-        for cx in range(9):
-            for cy in range(9):
+        for cx in range(self.size):
+            for cy in range(self.size):
+                # Number's different or it's the same square, so move on
                 if num != self[cy][cx] or (cx == x and cy == y):
                     continue
+                # On same row/col
                 if cx == x or cy == y:
                     return False
-                if cx//3 == x//3 and cy//3 == y//3:
+                # In same box
+                if cx//self.gridsize == x//self.gridsize and cy//self.gridsize == y//self.gridsize:
                     return False
         return True
     
     def issolved(self):
         """Tests if the current board is completely solved"""
-        for x in range(9):
-            for y in range(9):
+        for x in range(self.size):
+            for y in range(self.size):
                 if not self[y][x].solved:
                     return False
         return True
@@ -72,8 +103,8 @@ class board(list[list[square]]):
         then checks to see how that affects the others"""
         # Find the easiest square to solve (the one with the fewest options that isn't already solved)
         lowest_entropy = (-1, -1, 10000)
-        for x in range(9):
-            for y in range(9):
+        for x in range(self.size):
+            for y in range(self.size):
                 if len(self[y][x]) < lowest_entropy[2] and not self[y][x].solved:
                     lowest_entropy = (x, y, len(self[y][x]))
         sqr = self[lowest_entropy[1]][lowest_entropy[0]]
@@ -100,8 +131,8 @@ class board(list[list[square]]):
     def propagate(self):
         """Checks which numbers in the grid are no longer valid and removes them"""
         removed = []
-        for x in range(9):
-            for y in range(9):
+        for x in range(self.size):
+            for y in range(self.size):
                 sqr = self[y][x]
                 sqr_copy = copy.copy(sqr)
                 for num in sqr_copy:
@@ -111,28 +142,43 @@ class board(list[list[square]]):
         return removed
     
     def solve(self):
-        iter = 0
-        changestack = []
-        while not self.issolved():
-            iter += 1
-            print(f"Iteration #{iter}")
+        # Make a backup in case we get stuck
+        original = copy.deepcopy(self)
 
-            iter_res = self.iterate()
-            prop_res = self.propagate()
-            changestack.append((iter_res, prop_res))
+        # Try to solve it 3 times
+        for _ in range(3):
+            iter = 0
+            changestack = []
+            while (not self.issolved()) and iter<(self.size**2)*2:
+                iter += 1
+                print(f"Iteration #{iter}")
 
-            # Weird-looking list comprehension thing to check for any zeros
-            while len([row for row in self if len([itm for itm in row if len(itm)==0])]) != 0:
-                # There's a zero space, backtrack
-                print("Zero detected, removing incorrect choice")
-                last_iter, last_prop = changestack.pop()
-                print(f"Choice was {last_iter[2]} at ({last_iter[0]},{last_iter[1]})")
-                # Fix iteration
-                self[last_iter[1]][last_iter[0]] = square(last_iter[3])
-                self[last_iter[1]][last_iter[0]].solved = False
-                # Fix propagation
-                for rem in last_prop:
-                    self[rem[1]][rem[0]].append(rem[2])
+                # Iterates and remembers the changes that were made
+                iter_res = self.iterate()
+                prop_res = self.propagate()
+                changestack.append((iter_res, prop_res))
+
+                # Weird-looking list comprehension thing to check for any zeros
+                while len([row for row in self if len([itm for itm in row if len(itm)==0])]) != 0:
+                    # There's a zero space, backtrack
+                    print("Zero detected, removing incorrect choice")
+                    last_iter, last_prop = changestack.pop()
+                    print(f"Choice was {last_iter[2]} at ({last_iter[0]},{last_iter[1]})")
+                    # Fix iteration
+                    self[last_iter[1]][last_iter[0]] = square(last_iter[3])
+                    self[last_iter[1]][last_iter[0]].solved = False
+                    # Fix propagation
+                    for rem in last_prop:
+                        self[rem[1]][rem[0]].append(rem[2])
+            if self.issolved():
+                return
+            
+            # Hopefully, HOPEFULLY, this part will never be relevant. If it is, something's gone wrong.
+            print("Failed, trying again")
+            self.clear()
+            self.extend(copy.deepcopy(original))
+        # Even more hopefully, this will never trigger. When it does, I won't know what to do about it.
+        raise Exception("Houston, we have a problem. (Couldn't solve the board after 3 tries)")
 
 
 def main():
@@ -147,10 +193,10 @@ def main():
         [-1, -1, -1,  4,  1,  9, -1, -1,  5],
         [-1, -1, -1, -1,  8, -1, -1,  7,  9],
     ]
-    #board_arr = [[-1 for i in range(9)] for j in range(9)]
-    b = board()#board_arr)
+    b = board(board_arr)
     b.solve()
     print(b)
+    print(f"{b:compact}")
 
 if __name__ == "__main__":
     main()
